3dee166dd0441192d5f4676c11c4d07b24fd05c6
Poorva5 <92199944+Poorva5@users.noreply.github.com>
2022-11-29 12:48:19 +0530

    Crated Navbar component

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import SideDrawer from "../Components/Drawer/SideBar";
+
+const Home = () => {
+
+    return (
+        <div>
+            <SideDrawer />
+        </div>
+    )
+}
+
+export default Home;
